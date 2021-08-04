b69f085e3dba2746997e3a74403a62046cee871d
lambo10 <nnadi406@gmail.com>
2021-08-05 00:21:26 +0100

    new commit

--- src/components/modals/loginModal.js
+++ src/components/modals/loginModal.js
@@ -1,6 +1,6 @@
 
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+// import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import {loginModal_action} from '../../actions'
 import './loginModal.css';
 
